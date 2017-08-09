dd9c7faef0c5086b47c73bb118197c7d38f51f36
Christopher Sardina <chrissardina@Chris-Sardina.local>
2017-08-09 17:53:03 -0400

    8/8 Daily Project: Finished and tested

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
+// +++++ INITIAL VARIABLES +++++
+
 const main = document.querySelector('.main');
 const recipeCard = document.querySelectorAll('form')[0];
 const baseUrl = 'https://proxy.calweb.xyz/http://www.recipepuppy.com/api/';
 
+//=====================================================================================
+//++++++++++++++++++++ADDS GENERIC PLACEHOLDERS BEFORE SEARCH++++++++++++++++++++++++++
+//=====================================================================================
+let i = 8;
+let placeholder = '';
+while (i > 0){
+  let placeHolderRecipe = `
+      <section id="remove" class="recipeCard">
+        <img src="https://lh3.googleusercontent.com/dutChlaXdyAFMiyXSqUl79PSqrYpFpE7NpuDMDd4Uiq28XbqmkNxJJe953OkWxf5oTc=w300" alt="Recipe Immage">
+        <h4>Search For Your Favorite Recipes</h4>
+      </section>
+      `;
+      placeholder += placeHolderRecipe;
+      i--;
+}
+    main.innerHTML = placeholder;
+
+//=========================================================================================
+//+++++++++++++++++++++++++++MAIN SEARCH FUNCTION +++++++++++++++++++++++++++++++++++++++++
+//=========================================================================================
 recipeCard.addEventListener('submit', function (event) {
 
   event.preventDefault()
@@ -10,9 +32,8 @@ recipeCard.addEventListener('submit', function (event) {
 
   const searchWords = event.target.querySelector('input[name="searchTerm"]').value
 
-  let allHTML = `<h1 class="searchWords">Here are your search results for ${searchWords}</h1>`;
+  let allHTML = `<h1 class="searchWords">Recipe results for ${searchWords}</h1>`;
     // console.log("My search word: " + searchWords);
-    // console.log("======================");
 
   searchRecipePuppy(searchWords).then(function (results) {
     // console.log("TURN BELOW INTO A CONST");
@@ -24,35 +45,49 @@ recipeCard.addEventListener('submit', function (event) {
       const pass = results.results[i];
 
       function buildRecipe(){
-
-        let recipe = `
-            <section class="recipeCard">
-              <a href="${pass.href}">
-              <img src="${pass.thumbnail}" alt="Recipe Immage">
-              <h4>${pass.title}</h4>
-              </a>
-            </section>
-            `
-            return recipe;
+        if (pass.thumbnail === "") {
+          let recipe = `
+              <section class="recipeCard">
+                <a href="${pass.href}">
+                <img src="https://s-media-cache-ak0.pinimg.com/736x/3a/c1/96/3ac19695e3ea15750552151e89cd148f--kitty-litter-cake-chocolate-cake-mixes.jpg" alt="Recipe Immage">
+                <h4>${pass.title}</h4>
+                </a>
+              </section>
+              `
+              return recipe;
         }
+        else {
+          let recipe = `
+              <section class="recipeCard">
+                <a href="${pass.href}">
+                <img src="${pass.thumbnail}" alt="Recipe Immage">
+                <h4>${pass.title}</h4>
+                </a>
+              </section>
+              `
+              return recipe;
+        }
+      }
+
         allHTML += buildRecipe();
 
-    }
-console.log(allHTML);
+    } //end for loop
+    console.log(allHTML);
 
   main.innerHTML = allHTML;
-    // console.log(allHTML);
 
-    event.target.querySelector('input[name="searchTerm"]').value = '';
+  event.target.querySelector('input[name="searchTerm"]').value = '';
+
   })
 })
 
-//FETCH FUNCTION Outside function calledback in recipeCard event listener
+//=========================================================================================
+//+++++++++FETCH FUNCTION Outside function calledback in recipeCard event listener+++++++++
+//=========================================================================================
 let searchRecipePuppy = function(inputWords) {
   return fetch(`${baseUrl}?q=${encodeURIComponent(inputWords)}`).then(function(response) {
     return response.json();
     // console.log("DATA FROM FETCH AS DATA VARIABLE BEFORE RETURNED:");
     // console.log(data);
-    // console.log("=======================");
   })
 }
